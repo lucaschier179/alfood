a9ff3803ab23a1153b66901acc9212f9f76b06a3
lucaschier <ens-lucasschier@ugv.edu.br>
2023-08-11 14:24:10 -0300

    style: :lipstick: Ajuste no estilo com tailwind

--- src/components/ListaRestaurantes/Restaurante/index.tsx
+++ src/components/ListaRestaurantes/Restaurante/index.tsx
@@ -20,7 +20,7 @@ export default function Restaurante({ restaurante }: RestauranteProps) {
 
   return (
     <section className="mt-8 p-[30px] bg-[#f2f6ff]">
-      <div className="{estilos.Titulo} text-[28px] font-medium leading-6 pb-[15px] relative w-auto box-border inline-block">
+      <div className="text-[28px] font-medium leading-6 pb-[15px] relative w-auto box-border inline-block">
         <h2 className="m-0 w-auto 
         before:absolute before:left-0 before:bottom-0 before:h-[5px] before:w-[55px] before:bg-[#111] after:absolute after:left-0 after:bottom-[2px] after:h-[1px] 
         after:w-full after:max-w-full after:bg-[#333]"
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,51 +1,69 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import Banner from '../../components/Banner';
 import NavBar from '../../components/NavBar';
 import Rodape from '../../components/Rodape';
-import estilos from './Home.module.scss';
 
-function App() {
+export default function App() {
   return (
     <>
       <NavBar />
       <Banner />
-      <div className={estilos.MiniBanners}>
-        <img src="/imagens/cozinhar_01.jpg" alt="Um prato conceitual" />
-        <div className={estilos.CardCentral}>
-          <h2>A melhor rede de restaurantes!</h2>
+      <div className="flex justify-center items-center my-8 mx-0 flex-wrap">
+        <img src="/imagens/cozinhar_01.jpg" alt="Um prato conceitual" className="max-h-[185px]" />
+        <div className="text-center p-[30px] text-[#fff] bg-[#f2745f]">
+          <h2 className="max-w-[70%] inline-block">
+            A melhor rede de restaurantes!
+          </h2>
           <div>
-            <p>seja um parceiro agora:</p>
-            <p>ligue para <a href="callto:99999999999">(99) 99999-999</a></p>
+            <p>
+              seja um parceiro agora:
+            </p>
+            <p>
+              ligue para
+              <a href="callto:99999999999" className="no-underline text-[#631406] font-bold">
+                (99) 99999-999
+              </a>
+            </p>
           </div>
         </div>
-        <img src="/imagens/cozinhar_02.jpg" alt="Um hambúrguer desconstruído" />
+        <img src="/imagens/cozinhar_02.jpg" alt="Um hambúrguer desconstruído" className="max-h-[185px]" />
       </div>
-      <div className={estilos.Categorias}>
-        <div className={estilos.TipoDePrato}>
-          <img src="/imagens/cafedamanha.png" alt="Café da manhã" />
-          <h4>Café da manhã</h4>
+      <div className="flex justify-evenly flex-wrap items-center">
+        <div className="text-center">
+          <img src="/imagens/cafedamanha.png" alt="Café da manhã" className="max-w-[100%] overflow-hidden rounded-[50%] border-[10px] border-solid border-[#b4b4b4]" />
+          <h4 className="mt-5 text-[22px] mb-0 font-semibold text-[#121212]">
+            Café da manhã
+          </h4>
         </div>
-        <div className={estilos.TipoDePrato}>
-          <img src="/imagens/almoco.png" alt="Almoço" />
-          <h4>Almoço</h4>
+        <div className="text-center">
+          <img src="/imagens/almoco.png" alt="Almoço" className="max-w-[100%] overflow-hidden rounded-[50%] border-[10px] border-solid border-[#b4b4b4]" />
+          <h4 className="mt-5 text-[22px] mb-0 font-semibold text-[#121212]">
+            Almoço
+          </h4>
         </div>
-        <div className={estilos.TipoDePrato}>
-          <img src="/imagens/jantar.png" alt="Jantar" />
-          <h4>Jantar</h4>
+        <div className="text-center">
+          <img src="/imagens/jantar.png" alt="Jantar" className="max-w-[100%] overflow-hidden rounded-[50%] border-[10px] border-solid border-[#b4b4b4]" />
+          <h4 className="mt-5 text-[22px] mb-0 font-semibold text-[#121212]">
+            Jantar</h4>
         </div>
-        <div className={estilos.TipoDePrato}>
-          <img src="/imagens/sobremesa.png" alt="Sobremesas" />
-          <h4>Sobremesas</h4>
+        <div className="text-center">
+          <img src="/imagens/sobremesa.png" alt="Sobremesas" className="max-w-[100%] overflow-hidden rounded-[50%] border-[10px] border-solid border-[#b4b4b4]" />
+          <h4 className="mt-5 text-[22px] mb-0 font-semibold text-[#121212]">
+            Sobremesas
+          </h4>
         </div>
       </div>
-      <div className={estilos.Links}>
-        <h3>Conheça os melhores restaurantes</h3>
-        <p>Clique <Link to='/restaurantes'>aqui</Link></p>
+      <div className="text-center my-10 mx-0">
+        <h3>
+          Conheça os melhores restaurantes
+        </h3>
+        <p>
+          <Link to='/restaurantes'>
+            Clique aqui
+          </Link>
+        </p>
       </div>
       <Rodape />
     </>
   );
 }
-
-export default App;
