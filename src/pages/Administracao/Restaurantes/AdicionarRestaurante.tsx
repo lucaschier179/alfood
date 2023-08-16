7c35546415bc91553a0bb4048f3cda1e9ef61159
lucaschier <ens-lucasschier@ugv.edu.br>
2023-08-16 11:58:31 -0300

    style: :art: Upgrade na administração dos restaurantes

--- src/pages/Administracao/Restaurantes/AdicionarRestaurante.tsx
+++ src/pages/Administracao/Restaurantes/AdicionarRestaurante.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Link, Paper, TextField, Toolbar, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link as RouterLink, useParams } from "react-router-dom";
 import http from "../../../components/http";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
@@ -32,7 +32,7 @@ export default function AdicionarRestaurante() {
           const MensagemErro = error.response.data.nome
           alert(MensagemErro)
         })
-      } else {
+    } else {
       http.post("restaurantes/", {
         nome: nomeRestaurante
       })
@@ -44,31 +44,60 @@ export default function AdicionarRestaurante() {
           const MensagemErro = error.response.data.nome
           alert(MensagemErro)
         })
-      }
     }
+  }
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <Typography component="h1" variant="h6">Adicionar Restaurantes</Typography>
-      <Box component="form" onSubmit={handleFormSubmit}>
-        <TextField
-          id="standard-basic"
-          label="Nome do Restaurante"
-          variant="standard"
-          value={nomeRestaurante}
-          onChange={e => setNomeRestaurante(e.target.value)}
-          fullWidth
-          required
-        />
-        <Button
-          sx={{ marginTop: 2 }}
-          type="submit"
-          variant="contained"
-          fullWidth
-        >
-          Salvar
-        </Button>
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h6">
+              Administração
+            </Typography>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              <Link component={RouterLink} to={"/admin/restaurantes"}>
+                <Button sx={{ my: 2, color: "white" }}>
+                  Restaurantes
+                </Button>
+              </Link>
+              <Link component={RouterLink} to={"/admin/restaurantes/novo"}>
+                <Button sx={{ my: 2, color: "white" }}>
+                  Novo Restaurante
+                </Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 2 }}>
+          <Paper sx={{ p: 2 }}>
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
+              <Typography component="h1" variant="h6">Adicionar Restaurantes</Typography>
+              <Box sx={{ width: "100%" }} component="form" onSubmit={handleFormSubmit}>
+                <TextField
+                  id="standard-basic"
+                  label="Nome do Restaurante"
+                  variant="standard"
+                  value={nomeRestaurante}
+                  onChange={e => setNomeRestaurante(e.target.value)}
+                  fullWidth
+                  required
+                />
+                <Button
+                  sx={{ marginTop: 2 }}
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                >
+                  Salvar
+                </Button>
+              </Box>
+            </Box>
+          </Paper>
+        </Container>
       </Box>
-    </Box>
+    </>
   )
 }
